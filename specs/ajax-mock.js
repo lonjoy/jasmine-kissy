8ddd6de29bc5a2daffe4874934f796e83ef04bfa
minghe <minghe36@126.com>
2012-11-26 11:25:38 +0800

    add file

---
+++ specs/ajax-mock.js
@@ -0,0 +1,9 @@
+KISSY.add(function (S, Node,ServiceHelp) {
+    describe('kissy ajax mock test',function(){
+          it('use mock',function(){
+              
+          })
+    });
+},{requires:['node',
+    'specs/datas/serviceHelp'
+]});
---
+++ specs/runner.js
@@ -0,0 +1,14 @@
+/**
+ * @fileoverview 运行测试用例
+ * @author 剑平（明河）<minghe36@gmail.com>
+ **/
+KISSY.add(function (S, Base, Node) {
+    function runner(){
+        var env = jasmine.getEnv();
+        env.addReporter(new jasmine.HtmlReporter);
+        env.execute();
+    }
+    return runner;
+},{requires:['base', 'node',
+    'specs/proofList-spec'
+]});
